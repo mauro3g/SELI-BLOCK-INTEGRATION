c1423ae2f97a327fc80d5d6e42091ab9c41651d1
Mauro Alejandro Rivadeneira Maldonado <mauro3g@hotmail.com>
2019-09-16 15:08:30 -0500

    rest certificates

---
+++ SELI-Platform/lib/CertificatesCollection.js
@@ -0,0 +1,1 @@
+export const Certificates = new Mongo.Collection('certificates');
--- SELI-Platform/server/main.js
+++ SELI-Platform/server/main.js
@@ -11,7 +11,44 @@ import '../lib/PeopleCollection';
 import '../lib/extract';
 import '../lib/validateSignUp';
 import '../lib/usersUtil';
+import { Certificates } from '../lib/CertificatesCollection';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+if (Meteor.isServer) {
+
+  Meteor.startup(() => {
+
+    options={
+      url: "certificate-result",
+      getArgsFromRequest: function (request) {
+        var content = request.body;
+        // Since form enconding doesn't distinguish numbers and strings, we need
+        // to parse it manually
+        //console.log(parseInt(content.certificateNumber,10));
+        //console.log(content.certificateHash);
+        return [ content.certificateNumber, content.certificateHash ];
+      } 
+    }
+    
+    SimpleRest.setMethodOptions('certificate-result', options);
+
+    Meteor.methods({
+      'certificate-result': function (certificateNumber,certificateHash) {
+
+        console.log(certificateNumber);
+        console.log(certificateHash);
+
+        certificateInformation = {
+          number : certificateNumber,
+          hash : certificateHash
+        }
+
+        if(Certificates.insert(certificateInformation)){
+          return "Certificado registrado";
+        } else{
+          return "Error de registro";
+        }
+    },
+    });
+
+  });
+}
